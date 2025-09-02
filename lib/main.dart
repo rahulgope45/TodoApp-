ec71075b6ebcbfb2f890b9b7d4bd1dc40e8803ff
rahulgope45 <rahuleile456@gmail.com>
2025-09-02 22:32:50 +0530

    Added Todo to the app name and added an app icon

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, 
-      title: 'Todo App',
+      title: 'Todo',
       
-      home: const TodoScreen(),
+      home:  TodoScreen(),
     );
   }
 }
